0cd75531de8e25f27ca7cdef16e1297b881e05b8
emiru73 <moon1113uranus@ezweb.ne.jp>
2023-11-21 14:35:42 +0900

    追加

--- sakuhinn.py
+++ sakuhinn.py
@@ -1,3 +1,22 @@
-print(
-    
-)
+import tkinter
+import random
+
+root=tk.Tk()
+root.geometry('700x560')
+root['bg']='lightgrey'
+#画像用のキャンバス作成
+canvas=tk.Canvas(root,width=640,height=426,bd=0, highlightthickness=0, relief='ridge')
+#キャンバスを設置(上下に20の余白)
+canvas.pack(pady=20)
+#画像を用意
+photo1=tk.PhotoImage(file='cat1.png')
+#画像を描画(中点x,中点y,画像)
+canvas.create_image(320,213,image=photo1)
+photo2=tk.PhotoImage(file='cat2.png')
+canvas.create_image(340,233,image=photo2)
+root.mainloop()
+
+
+    root = tkinter.Tk()
+    root.geometry('1270x270')
+    q1 = tkinter.Label(root,text='このポケモンだ～れだ！')
